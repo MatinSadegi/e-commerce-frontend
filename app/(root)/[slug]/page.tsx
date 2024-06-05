ef1bb3880ab1fc6bc2a66b52bcd2941551e9d5ca
Matin Sadeghi <matinsadegi3290@gmail.com>
2024-06-05 12:33:20 +0330

    test commit

--- app/(root)/[slug]/page.tsx
+++ app/(root)/[slug]/page.tsx
@@ -6,7 +6,6 @@ import starIcon from "@/public/icons/star-favorite-1499-svgrepo-com.svg";
 import Image from "next/image";
 import Link from "next/link";
 import AddToCartButtons from "./AddToCartButtons";
-export const dynamic = "force-static";
 
 const openSans = Open_Sans({
   subsets: ["latin"],
@@ -14,19 +13,19 @@ const openSans = Open_Sans({
   display: "swap",
 });
 
-const Product = async ({ params }: { params: { slug: string } }) => {
-  const { slug } = params;
-  const {
-    image,
-    title,
-    price,
-    description,
-    quantity,
-    category,
-    subcategory,
-    brand,
-    _id,
-  } = await getProductBySlug(slug);
+const Product = async () => {
+  // const { slug } = params;
+  // const {
+  //   image,
+  //   title,
+  //   price,
+  //   description,
+  //   quantity,
+  //   category,
+  //   subcategory,
+  //   brand,
+  //   _id,
+  // } = await getProductBySlug(slug);
 
   return (
     <div
@@ -43,25 +42,25 @@ const Product = async ({ params }: { params: { slug: string } }) => {
       </div>
       <div className="flex flex-col justify-center md:flex-row md:space-x-7">
         <div className="md:w-1/2">
-          <Image
+          {/* <Image
             src={image.url}
             width={450}
             height={450}
             alt={title}
             className=" rounded"
-          />
+          /> */}
         </div>
         <div className=" mt-8 flex flex-col gap-6 md:w-1/2 md:m-0">
-          <h4 className=" text-gray-500 font-normal"> {title}</h4>
+          {/* <h4 className=" text-gray-500 font-normal"> {title}</h4> */}
           <div className="flex items-center gap-1">
             <Image src={starIcon} alt="star-icon" />
             <Image src={starIcon} alt="star-icon" />
             <Image src={starIcon} alt="star-icon" />
             <Image src={starIcon} alt="star-icon" />
           </div>
-          <p className="font-medium text-lg">${price.toFixed(2)}</p>
+          {/* <p className="font-medium text-lg">${price.toFixed(2)}</p>
           <p className="text-gray-500 leading-5">{description}</p>
-          <AddToCartButtons count={quantity} productId={_id} />
+          <AddToCartButtons count={quantity} productId={_id} /> */}
         </div>
       </div>
       <div className=" w-full border border-gray-300 mt-10">
@@ -74,18 +73,18 @@ const Product = async ({ params }: { params: { slug: string } }) => {
           <div className="grid grid-row-3 border-y border-gray-300">
             <div className="flex ">
               <p className="w-1/3 p-2.5 font-bold ">Compositions</p>
-              <p className="border-l p-2.5 border-gray-300">{brand}</p>
+              {/* <p className="border-l p-2.5 border-gray-300">{brand}</p> */}
             </div>
             <div className="flex border-t border-gray-300">
               <p className="w-1/3 p-2.5 font-bold ">Styles</p>
-              <p className="border-l p-2.5 border-gray-300">{category}</p>
+              {/* <p className="border-l p-2.5 border-gray-300">{category}</p> */}
             </div>
             <div className="flex border-t border-gray-300">
               <p className="w-1/3 p-2.5 font-bold ">Properties</p>
-              <p className="border-l p-2.5 border-gray-300">{subcategory}</p>
+              {/* <p className="border-l p-2.5 border-gray-300">{subcategory}</p> */}
             </div>
           </div>
-          <p className=" mt-8">{description}</p>
+          {/* <p className=" mt-8">{description}</p> */}
         </div>
       </div>
     </div>
@@ -94,9 +93,9 @@ const Product = async ({ params }: { params: { slug: string } }) => {
 
 export default Product;
 
-export async function generateStaticParams() {
-  const products = await getProducts("");
-  return products.slice(0, 3).map((product: ProductType) => ({
-    slug: product.slug,
-  }));
-}
+// export async function generateStaticParams() {
+//   const products = await getProducts("");
+//   return products.slice(0, 3).map((product: ProductType) => ({
+//     slug: product.slug,
+//   }));
+// }
